94b2c40d912913edd78642f59524b9997737cdae
codeNinja62 <sameer.ahmed12141@gmail.com>
2025-10-25 11:36:40 +0500

    Feat: Add executeInstruction() declaration for single-cycle mode

--- src/core/SparseFlowCore.h
+++ src/core/SparseFlowCore.h
@@ -9,13 +9,18 @@ class SparseFlowCore {
 private:
     int pc;
     long long cycle_count;
+    bool halted;
     RegisterFile reg_file;
     Memory memory;
+
+    // Single-cycle execution
+    void executeInstruction(const Instruction& instr);
 
 public:
     SparseFlowCore();
     void loadProgram(const std::vector<Instruction>& prog);
     void run();
+    void writeDataMemory(int addr, int val);
 };
 
 #endif // SPARSE_FLOW_CORE_H
