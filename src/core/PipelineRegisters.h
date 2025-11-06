bda43a27ccd12b538164b2229e47bfd9af0879dc
codeNinja62 <sameer.ahmed12141@gmail.com>
2025-11-06 19:51:06 +0500

    Feat: Define ID/EX Register struct with read values

--- src/core/PipelineRegisters.h
+++ src/core/PipelineRegisters.h
@@ -7,9 +7,25 @@
 struct IF_ID_Reg {
     Instruction instr;
     int pc;
+    bool predicted_taken;
     bool valid;
     
-    IF_ID_Reg() : pc(0), valid(false) {}
+    IF_ID_Reg() : pc(0), predicted_taken(false), valid(false) {}
+};
+
+// ID/EX Pipeline Register
+struct ID_EX_Reg {
+    Instruction instr;
+    int pc;
+    int rs1_val;
+    int rs2_val;
+    int imm;
+    int rd;
+    bool predicted_taken;
+    bool valid;
+    
+    ID_EX_Reg() : pc(0), rs1_val(0), rs2_val(0), imm(0), rd(0), 
+                  predicted_taken(false), valid(false) {}
 };
 
 #endif // PIPELINE_REGISTERS_H
