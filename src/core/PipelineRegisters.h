cc96c51d98d033829beb88a4ad5128ac9801c2e8
codeNinja62 <sameer.ahmed12141@gmail.com>
2025-11-07 00:31:34 +0500

    Feat: Define EX/MEM and MEM/WB Register structs

--- src/core/PipelineRegisters.h
+++ src/core/PipelineRegisters.h
@@ -1,9 +1,9 @@
 #ifndef PIPELINE_REGISTERS_H
 #define PIPELINE_REGISTERS_H
 
+#include <vector>
 #include "../isa/Instruction.h"
 
-// IF/ID Pipeline Register
 struct IF_ID_Reg {
     Instruction instr;
     int pc;
@@ -13,7 +13,6 @@ struct IF_ID_Reg {
     IF_ID_Reg() : pc(0), predicted_taken(false), valid(false) {}
 };
 
-// ID/EX Pipeline Register
 struct ID_EX_Reg {
     Instruction instr;
     int pc;
@@ -24,8 +23,29 @@ struct ID_EX_Reg {
     bool predicted_taken;
     bool valid;
     
-    ID_EX_Reg() : pc(0), rs1_val(0), rs2_val(0), imm(0), rd(0), 
+    ID_EX_Reg() : pc(0), rs1_val(0), rs2_val(0), imm(0), rd(0),
                   predicted_taken(false), valid(false) {}
+};
+
+struct EX_MEM_Reg {
+    Instruction instr;
+    int alu_result;
+    int write_data;
+    int rd;
+    bool valid;
+    
+    EX_MEM_Reg() : alu_result(0), write_data(0), rd(0), valid(false) {}
+};
+
+struct MEM_WB_Reg {
+    Instruction instr;
+    int mem_data;
+    std::vector<int> vec_data;
+    int alu_result;
+    int rd;
+    bool valid;
+    
+    MEM_WB_Reg() : mem_data(0), alu_result(0), rd(0), valid(false) {}
 };
 
 #endif // PIPELINE_REGISTERS_H
