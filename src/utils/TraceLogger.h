16f11b3525ee8595e2091867539134a087034090
codeNinja62 <sameer.ahmed12141@gmail.com>
2025-11-20 13:12:39 +0500

    Feat: Define TraceLogger class for pipeline visualization

--- src/utils/TraceLogger.h
+++ src/utils/TraceLogger.h
@@ -12,7 +12,14 @@ private:
 public:
     TraceLogger(const std::string& filename);
     ~TraceLogger();
-    void logCycle(int cycle, const std::string& stage_info);
+    
+    void logCycle(int cycle, 
+                  const std::string& fetch_instr, 
+                  const std::string& decode_instr, 
+                  const std::string& execute_instr, 
+                  const std::string& memory_instr, 
+                  const std::string& writeback_instr,
+                  const std::string& event);
 };
 
 #endif // TRACE_LOGGER_H
